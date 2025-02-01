c7b3bfffcbc0f00d460d9f3ae214b3395d7b2cf1
kenDRoger <kengabriell.fajardo.cics@ust.edu.ph>
2025-02-01 14:15:12 +0800

    Finalized Code

--- databasehandlerlab/src/main/java/com/mycompany/project/DatabaseHandler.java
+++ databasehandlerlab/src/main/java/com/mycompany/project/DatabaseHandler.java
@@ -1,12 +1,11 @@
 package com.mycompany.project;
 
-import java.io.File;
 import java.nio.file.Paths;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class DatabaseHandler {
 
@@ -53,6 +52,14 @@ public class DatabaseHandler {
                 this.address = address;
             }
         }
+
+        @Override
+        public String toString() {
+            return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", middleName="
+                    + middleName + ", sex=" + sex + ", dateOfBirth=" + dateOfBirth + ", dateOfEnrollment="
+                    + dateOfEnrollment + ", department=" + department + ", units=" + units + ", address=" + address
+                    + "]";
+        }
     }
 
     public DatabaseHandler(String database) {
@@ -61,7 +68,6 @@ public class DatabaseHandler {
             this.con = DriverManager.getConnection(DB_URL);
             System.out.println("Connection to SQLite has been established using url: " + DB_URL);
         } catch (Exception e) {
-            // TODO: handle exception
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
     }
@@ -85,7 +91,7 @@ public class DatabaseHandler {
         PreparedStatement stmt = null;
         try {
             stmt = con.prepareStatement(
-                    "CREATE TABLE student (" + "student_id TEXT PRIMARY KEY CHECK (student_id LIKE '____010____'), "
+                    "CREATE TABLE students (" + "student_id TEXT PRIMARY KEY CHECK (student_id LIKE '____010____'), "
                             + "student_fname TEXT NOT NULL, " + "student_mname TEXT NOT NULL, "
                             + "student_lname TEXT NOT NULL, " + "student_sex TEXT CHECK (student_sex IN ('M', 'F')), "
                             + "student_birth TEXT CHECK (student_birth LIKE '____-__-__'), "
@@ -94,7 +100,7 @@ public class DatabaseHandler {
                             + "student_units INT CHECK (student_units > 0), " + "student_address TEXT NOT NULL" + ")");
             stmt.executeUpdate();
             // tester
-            System.out.println("\n\nCommand executed: " + stmt.toString());
+            System.out.println("\n\nCommand executed/initialized: " + stmt.toString());
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         } finally {
@@ -109,40 +115,15 @@ public class DatabaseHandler {
         }
     }
 
-    Boolean insertStudent(String id, String firstName, String lastName, String middleName, String sex,
-            String dateOfBirth, int dateOfEnrollment, String department, int units, String address) {
+    Student getStudent(String studentNumber) {
         try {
-            PreparedStatement stmt = con.prepareStatement("INSERT INTO student VALUES (?,?,?,?,?,?,?,?,?,?)");
-            stmt.setString(1, id);
-            stmt.setString(2, firstName);
-            stmt.setString(3, middleName);
-            stmt.setString(4, lastName);
-            stmt.setString(5, sex);
-            stmt.setString(6, dateOfBirth);
-            stmt.setInt(7, dateOfEnrollment);
-            stmt.setString(8, department);
-            stmt.setInt(9, units);
-            stmt.setString(10, address);
-            stmt.executeUpdate();
-
-            // tester
-            System.out.println("\nCommand executed: " + stmt.toString());
-            return true;
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + "/insertstudent: " + e.getMessage());
-        }
-        return false;
-    }
-
-    Student getStudent(String id) {
-        try {
-            PreparedStatement stmt = con.prepareStatement("SELECT * FROM student WHERE student_id = ?");
-            stmt.setString(1, id);
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM students WHERE student_id = ?");
+            stmt.setString(1, studentNumber);
             ResultSet rs = stmt.executeQuery();
             // tester
-            System.out.println("\nCommand executed: " + stmt.toString());
+            System.out.println("\nCommand executed/getstudent: " + stmt.toString());
             if (rs.next()) {
-                return new Student(id, rs.getString("student_fname"), rs.getString("student_mname"),
+                return new Student(studentNumber, rs.getString("student_fname"), rs.getString("student_mname"),
                         rs.getString("student_lname"), rs.getString("student_sex"), rs.getString("student_birth"),
                         rs.getInt("student_start"), rs.getString("student_department"), rs.getInt("student_units"),
                         rs.getString("student_address"));
@@ -153,32 +134,183 @@ public class DatabaseHandler {
         return null;
     }
 
-    // tester
-    void dropTable() {
+    Student getStudent(String studentFname, String studentMname, String studentLname) {
         try {
-            PreparedStatement stmt = con.prepareStatement("drop table student");
-            stmt.executeUpdate();
+            PreparedStatement stmt = con.prepareStatement(
+                    "SELECT * FROM students WHERE student_fname = ? AND student_mname = ? AND student_lname = ?");
+            stmt.setString(1, studentFname);
+            stmt.setString(2, studentMname);
+            stmt.setString(3, studentLname);
+            ResultSet rs = stmt.executeQuery();
+            // tester
+            System.out.println("\nCommand executed/getstudent: " + stmt.toString());
+            if (rs.next()) {
+                return new Student(rs.getString("student_id"), rs.getString("student_fname"),
+                        rs.getString("student_mname"), rs.getString("student_lname"), rs.getString("student_sex"),
+                        rs.getString("student_birth"), rs.getInt("student_start"), rs.getString("student_department"),
+                        rs.getInt("student_units"), rs.getString("student_address"));
+            }
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + "/getstudent: " + e.getMessage());
+        }
+        return null;
+    }
+
+    ArrayList<Student> getStudents() {
+        ArrayList<Student> students = new ArrayList<>();
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM students");
+            ResultSet rs = stmt.executeQuery();
             // tester
             System.out.println("\nCommand executed: " + stmt.toString());
+            while (rs.next()) {
+                Student student = new Student(rs.getString("student_id"), rs.getString("student_fname"),
+                        rs.getString("student_mname"), rs.getString("student_lname"), rs.getString("student_sex"),
+                        rs.getString("student_birth"), rs.getInt("student_start"), rs.getString("student_department"),
+                        rs.getInt("student_units"), rs.getString("student_address"));
+                students.add(student);
+            }
         } catch (Exception e) {
-            System.err.println(e.getClass().getName() + "/droptable: " + e.getMessage());
+            System.err.println(e.getClass().getName() + "/getstudents: " + e.getMessage());
+        }
+        return students;
+    }
+
+    Boolean removeStudent(String studentNumber) {
+        try {
+            PreparedStatement stmt = con.prepareStatement("DELETE FROM students WHERE student_id = ?");
+            stmt.setString(1, studentNumber);
+            stmt.executeUpdate();
+            // tester
+            System.out.println("\nCommand executed/removeStudent: " + stmt.toString());
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + "/removestudent: " + e.getMessage());
+            return false;
         }
     }
 
+    ArrayList<Student> getStudentsByYear(int year) {
+        ArrayList<Student> students = new ArrayList<>();
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM students WHERE student_start = ?");
+            stmt.setInt(1, year);
+            ResultSet rs = stmt.executeQuery();
+            // tester
+            System.out.println("\nCommand executed/getStudentsByYear: " + stmt.toString());
+            while (rs.next()) {
+                Student student = new Student(rs.getString("student_id"), rs.getString("student_fname"),
+                        rs.getString("student_mname"), rs.getString("student_lname"), rs.getString("student_sex"),
+                        rs.getString("student_birth"), rs.getInt("student_start"), rs.getString("student_department"),
+                        rs.getInt("student_units"), rs.getString("student_address"));
+                students.add(student);
+            }
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + "/getstudents: " + e.getMessage());
+        }
+        return students;
+    }
+
+    Boolean updateStudentInfo(String studentNumber, Student studentInfo) {
+        try {
+            PreparedStatement stmt = con.prepareStatement("UPDATE students SET student_fname = ?, student_mname = ?, "
+                    + "student_lname = ?, student_sex = ?, student_birth = ?, student_start = ?, student_department = ?, "
+                    + "student_units = ?, student_address = ? WHERE student_id = ?");
+            stmt.setString(1, studentInfo.firstName);
+            stmt.setString(2, studentInfo.middleName);
+            stmt.setString(3, studentInfo.lastName);
+            stmt.setString(4, studentInfo.sex);
+            stmt.setString(5, studentInfo.dateOfBirth);
+            stmt.setInt(6, studentInfo.dateOfEnrollment);
+            stmt.setString(7, studentInfo.department);
+            stmt.setInt(8, studentInfo.units);
+            stmt.setString(9, studentInfo.address);
+            stmt.setString(10, studentNumber);
+            stmt.executeUpdate();
+            // tester
+            System.out.println("\nCommand executed/updatestudentinfo: " + stmt.toString());
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + "/updatestudentinfo: " + e.getMessage());
+            return false;
+        }
+    }
+
+    Boolean updateStudentUnits(String studentNumber, int subtractedUnits) {
+        try {
+            PreparedStatement stmt = con.prepareStatement("UPDATE students SET student_units = ? WHERE student_id = ?");
+            int originalUnits = getStudent(studentNumber).units;
+            stmt.setInt(1, originalUnits - subtractedUnits);
+            stmt.setString(2, studentNumber);
+            stmt.executeUpdate();
+            // tester
+            System.out.println("\nCommand executed/updateStudentUnits: " + stmt.toString());
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + "/updatestudentinfo: " + e.getMessage());
+            return false;
+        }
+    }
+
+    Boolean insertStudent(Student newStudent) {
+        try {
+            PreparedStatement stmt = con.prepareStatement("INSERT INTO students VALUES (?,?,?,?,?,?,?,?,?,?)");
+            stmt.setString(1, newStudent.id);
+            stmt.setString(2, newStudent.firstName);
+            stmt.setString(3, newStudent.middleName);
+            stmt.setString(4, newStudent.lastName);
+            stmt.setString(5, newStudent.sex);
+            stmt.setString(6, newStudent.dateOfBirth);
+            stmt.setInt(7, newStudent.dateOfEnrollment);
+            stmt.setString(8, newStudent.department);
+            stmt.setInt(9, newStudent.units);
+            stmt.setString(10, newStudent.address);
+            stmt.executeUpdate();
+            // tester
+            System.out.println("\nCommand executed/insertstudent: " + stmt.toString());
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + "/insertstudent: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // test all methods
     public static void main(String[] args) {
         System.out.println("Database Path: " + DB_PATH);
         DatabaseHandler db = new DatabaseHandler(DEFAULT_DATABASE);
 
+        // init
         db.initializeStudents();
-        db.insertStudent("12340101234", "John", "D.", "Doe", "M", "2000-05-15", 2022, "College of Science", 21,
-                "123 Main Street, Manila");
-        System.out.println("Student: " + db.getStudent("12340101234"));
-        db.dropTable();
-
+        // insert
+        db.insertStudent(db.new Student("11110102222", "John", "Doe", "Doe", "M", "2000-01-01", 2017,
+                "College of Accountancy", 3, "123 Main St"));
+        db.insertStudent(db.new Student("33330104444", "Jane", "", "Doe", "F", "1990-01-01", 2017,
+                "College of Information and Computing Sciences", 3, "456 Main St"));
+        db.insertStudent(db.new Student("22220203333", "Alice", "Marie", "Smith", "F", "1995-02-15", 2016,
+                "Faculty of Arts and Letters", 4, "789 Main St"));
+        // updatestudentunits
+        db.updateStudentUnits("11110102222", 1);
+        // getstudents
+        db.getStudents().forEach(System.out::println);
+        // getstudentsbyyear
+        db.getStudentsByYear(2017).forEach(System.out::println);
+        // getstudent
+        DatabaseHandler.Student s1 = db.getStudent("John", "Doe", "Doe");
+        DatabaseHandler.Student s2 = db.getStudent("33330104444");
+        System.out.println("Student: " + s1);
+        System.out.println("Student: " + s2);
+        // removestudent
+        db.removeStudent("11110102222");
+        db.getStudents().forEach(System.out::println);
+        // updatestudentinfo
+        db.updateStudentInfo("33330104444", db.new Student("33330104444", "Jane", "Doe", "Doe", "F", "1990-01-01", 2018,
+                "College of Information and Computing Sciences", 6, "789 Main St"));
+        System.out.println("newStudent: " + db.getStudent("33330104444"));
         try {
             db.con.close();
         } catch (Exception e) {
-            // TODO: handle exception
+            System.err.println(e.getClass().getName() + "/main: " + e.getMessage());
         }
 
     }
